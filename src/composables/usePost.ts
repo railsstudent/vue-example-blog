b11e8b7b53d52816f72a30db8985e8f8896bfac3
Connie Leung <cleung334@gmail.com>
2025-09-07 22:32:25 +0800

    feat:  enhance usePost to fetch a post

--- src/composables/usePost.ts
+++ src/composables/usePost.ts
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 
 export function usePost() {
   const posts = ref<Post[]>([])
+  const post = ref<Post | null>(null)
 
   function fetchAll() {
     return fetch('https://jsonplaceholder.typicode.com/posts')
@@ -10,8 +11,16 @@ export function usePost() {
       .then((data) => (posts.value = data))
   }
 
+  function fetchOne(id: number) {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((response) => response.json() as Promise<Post>)
+      .then((data) => (post.value = data))
+  }
+
   return {
     posts,
+    post,
     fetchAll,
+    fetchOne,
   }
 }
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-const post = {
-  title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-  body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
-}
+import { useRoute } from 'vue-router'
+import { usePost } from '@/composables/usePost'
+
+const { post, fetchOne } = usePost()
+
+const { params } = useRoute();
+const postId = +params.id
+console.log(postId)
+
+fetchOne(postId)
+
 const user = {
   name: 'Leanne Graham',
 }
