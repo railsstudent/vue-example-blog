a2505b4415322231c7e685c96997dba7f1c769eb
Connie Leung <cleung334@gmail.com>
2025-09-14 12:41:07 +0800

    refactor: use useFetch to make network call

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
-import PostCard from '../components/PostCard.vue'
-import { useResource } from '@/composables/useResource'
 import type { Post } from '@/types/post'
+import { useFetch } from '@vueuse/core'
+import PostCard from '../components/PostCard.vue'
 
-const { items, fetchAll } = useResource<Post>('posts')
-
-fetchAll()
+const {
+  data: posts,
+  isFetching,
+  error,
+} = useFetch<Post[]>('https://jsonplaceholder.typicode.com/posts').json()
 </script>
 
 <template>
-  <div class="flex flex-wrap flex-grow">
-    <PostCard v-for="post in items" :key="post.id" :post="post" />
+  <div v-if="isFetching" class="text-center mb-10">Loading ...</div>
+  <div v-if="error" class="text-center mb-10">{{ error }}</div>
+  <div v-if="posts" class="flex flex-wrap flex-grow">
+    <p class="ml-2 w-full">Number of posts: {{ posts.length }}</p>
+    <PostCard v-for="post in posts" :key="post.id" :post="post" />
   </div>
 </template>
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,40 +1,41 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
-import { useResource } from '@/composables/useResource'
-import { watch } from 'vue'
 import type { Post } from '@/types/post'
 import type { User } from '@/types/user'
-import { usePageRequests } from '@/composables/userPageRequests'
+import { useFetch } from '@vueuse/core'
+import { computed, shallowRef, watch } from 'vue'
+import { useRoute } from 'vue-router'
 
-const { isLoading } = usePageRequests()
-const { item: post, fetchOne } = useResource<Post>('posts')
-const { item: user, fetchOne: fetchUser } = useResource<User>('users')
+const baseUrl = 'https://jsonplaceholder.typicode.com'
 
 const { params } = useRoute()
+const url = `${baseUrl}/posts/${params.id}`
 
-fetchOne(+params.id)
+const { data: post, isFetching: isFetchingPost, error: errorPost } = useFetch<Post>(url).json()
+
+const userUrl = shallowRef('')
+
+const {
+  data: user,
+  isFetching: isFetchingUser,
+  error: errorUser,
+} = useFetch<User>(userUrl, { refetch: true, immediate: false }).json()
+
+const isFetching = computed(() => isFetchingPost.value || isFetchingUser.value)
 
 watch(
   () => ({ ...post.value }),
-  (newPost, oldPost, onCleanup) => {
-    const controller = new AbortController()
-
-    if (newPost?.userId) {
-      fetchUser(newPost.userId, controller.signal)
-    }
-
-    onCleanup(() => {
-      controller.abort()
-    })
-  },
+  ({ userId = undefined }) => (userUrl.value = userId ? `${baseUrl}/users/${userId}` : ''),
 )
 </script>
 
 <template>
-  <div v-if="isLoading" class="text-center my-10">Loading...</div>
+  <div v-if="isFetching" class="text-center my-10">Loading...</div>
+  <div v-if="errorPost || errorUser" class="text-center my-10">
+    Error{{ errorPost || errorUser }}
+  </div>
   <div v-if="post && user" class="mb-10">
     <h1 class="text-3xl">{{ post.title }}</h1>
-    <div class="text-gray-500 mb-10">by {{ user.name }}</div>
+    <div class="text-gray-500 mb-10">by {{ user?.name }}</div>
     <div class="mb-10">{{ post.body }}</div>
   </div>
 </template>
